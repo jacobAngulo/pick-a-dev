3fb7dcb368a1497bdf44ce74916ac7387bbf1f18
Jacob Angulo <jacobangulo@hotmail.com>
2019-06-13 20:00:11 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const display = document.querySelector(".dev");
+const button = document.querySelector(".picker");
+
+const devs = [
+  "Aiden",
+  "Carl",
+  "Daniel",
+  "Gabriel",
+  "Liam",
+  "Patrick",
+  "Toosdai"
+];
+
+display.textContent = "press the button to pick a dev";
+
+button.addEventListener("click", () => {
+  devs.sort(() => Math.random() - 0.5);
+  display.textContent = devs[0] == display.textContent ? devs[1] : devs[0];
+});
